1c4a710f35fdc74c85c143d5e29ad448d46d39bd
ChaosCedd <166236276+icodecedd@users.noreply.github.com>
2025-08-10 14:54:38 +0800

    feat(helpers): refactor the metadata and utilities for better performance.

--- client/src/constants/activityLogsMeta.js
+++ client/src/constants/activityLogsMeta.js
@@ -2,63 +2,63 @@ export const activityMeta = {
   REQUESTS: {
     APPROVED: {
       title: "Approved Request",
-      description: "Request for equipment was approved"
+      description: "Request for equipment was approved",
     },
     REJECTED: {
       title: "Rejected Request",
-      description: "Request for equipment was rejected"
+      description: "Request for equipment was rejected",
     },
     CREATED: {
       title: "Created Request",
-      description: "A new request was submitted for equipment"
+      description: "A new request was submitted for equipment",
     },
     COMPLETED: {
       title: "Returned Equipment",
-      description: "Borrowed equipment was marked as returned"
+      description: "Borrowed equipment was marked as returned",
     },
     CANCELLED: {
       title: "Cancelled Request",
-      description: "A request was cancelled by the user"
-    }
+      description: "A request was cancelled by the user",
+    },
   },
   EQUIPMENT: {
     CREATED: {
       title: "Added Equipment",
-      description: "New equipment was added to the system"
+      description: "New equipment was added to the system",
     },
     UPDATED: {
       title: "Updated Equipment",
-      description: "Equipment details were updated"
+      description: "Equipment details were updated",
     },
     DELETED: {
       title: "Deleted Equipment",
-      description: "An equipment item was removed from the system"
+      description: "An equipment item was removed from the system",
     },
   },
   USERS: {
     CREATED: {
       title: "Added User",
-      description: "A new user was added to the system"
+      description: "A new user was added to the system",
     },
     UPDATED: {
       title: "Updated User",
-      description: "User details were updated"
+      description: "User details were updated",
     },
     DELETED: {
       title: "Deleted User",
-      description: "A user was removed from the system"
+      description: "A user was removed from the system",
     },
     REACTIVATED: {
       title: "Activated User",
-      description: "A user was activated in the system"
+      description: "A user was activated in the system",
     },
     DEACTIVATED: {
       title: "Deactivated User",
-      description: "A user was deactivated in the system"
+      description: "A user was deactivated in the system",
     },
     PASSWORD_RESET: {
       title: "Reset Password",
-      description: "A user's password was reset"
-    }
-  }
+      description: "A user's password was reset",
+    },
+  },
 };
