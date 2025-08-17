3e7ff77eb11a2b790c81354f283f20a6d56c2d6b
ChaosCedd <166236276+icodecedd@users.noreply.github.com>
2025-08-17 23:27:30 +0800

    feat(ui): initially design the tab lists ui for admin settings

--- client/src/pages/admin/Settings/AdminSettingsTable.jsx
+++ client/src/pages/admin/Settings/AdminSettingsTable.jsx
@@ -1,5 +1,17 @@
-import { Box, Tabs, TabList, Tab, TabPanels, TabPanel, Button, Flex } from "@chakra-ui/react";
-import { FiSave } from "react-icons/fi";
+import {
+  Box,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Button,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { AiFillNotification } from "react-icons/ai";
+import { FiBell, FiDatabase, FiGlobe, FiSave, FiShield } from "react-icons/fi";
 
 const AdminSettingsTable = () => {
   return (
@@ -44,12 +56,58 @@ const AdminSettingsTable = () => {
           ))}
         </TabList>
         <TabPanels>
-          <TabPanel>
-            General
+          <TabPanel mt={2} p={0}>
+            <Box borderRadius="md" border={"1px solid #e2e8f0"} p={3}>
+              <Flex gap={3} alignItems={"center"}>
+                <FiGlobe size="22px" />
+                <Heading fontSize="25px" fontWeight={"semibold"}>
+                  General Settings
+                </Heading>
+              </Flex>
+              <Text fontSize="sm" mb={2} color="#71717e">
+                Configure basic system settings and preferences
+              </Text>
+            </Box>
           </TabPanel>
-          <TabPanel>Notifications</TabPanel>
-          <TabPanel>Security</TabPanel>
-          <TabPanel>System</TabPanel>
+          <TabPanel mt={2} p={0}>
+            <Box borderRadius="md" border={"1px solid #e2e8f0"} p={3}>
+              <Flex gap={3} alignItems={"center"}>
+                <FiBell size="22px" />
+                <Heading fontSize="25px" fontWeight={"semibold"}>
+                  Notification
+                </Heading>
+              </Flex>
+              <Text fontSize="sm" mb={2} color="#71717e">
+                Configure how and when you receive notifications
+              </Text>
+            </Box>
+          </TabPanel>
+          <TabPanel mt={2} p={0}>
+            <Box borderRadius="md" border={"1px solid #e2e8f0"} p={3}>
+              <Flex gap={3} alignItems={"center"}>
+                <FiShield size="22px" />
+                <Heading fontSize="25px" fontWeight={"semibold"}>
+                  Security Settings
+                </Heading>
+              </Flex>
+              <Text fontSize="sm" mb={2} color="#71717e">
+                Configure security policies and access controls
+              </Text>
+            </Box>
+          </TabPanel>
+          <TabPanel mt={2} p={0}>
+            <Box borderRadius="md" border={"1px solid #e2e8f0"} p={3}>
+              <Flex gap={3} alignItems={"center"}>
+                <FiDatabase size="22px" />
+                <Heading fontSize="25px" fontWeight={"semibold"}>
+                  System Settings
+                </Heading>
+              </Flex>
+              <Text fontSize="sm" mb={2} color="#71717e">
+                Configure system settings and preferences
+              </Text>
+            </Box>
+          </TabPanel>
         </TabPanels>
       </Tabs>
     </Box>
